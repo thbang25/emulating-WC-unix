bb2b39ba0956a40d17c5aeb192879efe6498c85f
thbang25 <tpstyles389@gmail.com>
2024-02-21 17:48:50 +0200

    defined the function body to create a character table

--- counts.h
+++ counts.h
@@ -11,7 +11,7 @@ replicate wc **/
 struct CharInfo {char character; long count; }; //data type for character frequency table
 std::vector<CharInfo> characterFrequency (const std::vector<std::string>& data); //character frequency table vector
 int WordCounterPerline(const std::string& fileData); //we looking through each line
-int WordCounter(const std::vector<std::string>& data); //sum the count
+int WordCounter(const std::vector<std::string>& data); //sum the counts
 int charCounter(const std::vector<std::string>& data); //count characters
 bool ContainsNonSpecial(const std::string& data);//check if the word in the line contains non special character
 int charCounterPerline(const std::string& fileData); //count the number of charecters
